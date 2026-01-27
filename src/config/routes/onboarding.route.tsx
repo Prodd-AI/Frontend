603da24008adaca636fb4d1926e310c8eea3cc41
Wizzyszn <wisdomolubayoadedoyin@yahoo.com>
2026-01-27 16:51:48 +0100

    feat(onboarding): refactor onboarding flow and team lead setup

--- src/config/routes/onboarding.route.tsx
+++ src/config/routes/onboarding.route.tsx
@@ -1,21 +1,21 @@
-import TeamLeadSetup from "@/onboarding/pages/team-lead/set.team-lead.page";
+import TeamLeadSetup from "@/onboarding/pages/team-lead/setup.team-lead.page";
 import { lazy } from "react";
 import type { RouteObject } from "react-router-dom";
 import withAuthGuard from "@/shared/components/HOC/with-auth-guard";
 
-const SelectRole = lazy(() => import("@/onboarding/pages/select-role.page"));
+// const SelectRole = lazy(() => import("@/onboarding/pages/select-role.page"));
 
 const HrSetup = lazy(() => import("@/onboarding/pages/hr/setup.hr.page"));
 
-const GuardedSelectRole = withAuthGuard(SelectRole);
+// const GuardedSelectRole = withAuthGuard(SelectRole);
 const GuardedHrSetup = withAuthGuard(HrSetup);
 const GuardedTeamLeadSetup = withAuthGuard(TeamLeadSetup);
 
 export const onboarding_routes: RouteObject[] = [
-  {
-    path: "select-role",
-    element: <GuardedSelectRole />,
-  },
+  // {
+  //   path: "select-role",
+  //   element: <GuardedSelectRole />,
+  // },
   {
     path: "hr-setup",
     element: <GuardedHrSetup />,
